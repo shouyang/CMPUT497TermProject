5dd1f5febd9d497fbb6c8c08187fd01fd1cc7e56
Shouyang Zhou <s.zhou1511@gmail.com>
2019-11-02 18:22:33 -0600

    Made initial interface 4 accessing test abstracts

--- scripts/loadData.py
+++ scripts/loadData.py
@@ -4,7 +4,9 @@ import json
 
 
 class Abstract:
-    def __init__(self, filepath):
+    """ Class used to denote a research paper abstract to be used as sample for evaluating keyword extraction methods.
+    """
+    def __init__(self, filepath, keyword_reference):
         # Absolute Filepath used to create this object.
         self.filepath = os.path.abspath(filepath)
         # File name of this item, should be an integer
@@ -14,7 +16,7 @@ class Abstract:
         # String-paragraph denoting the actual contents of the file.
         self.sentence_tokens, self.abstract = self.gen_abstract()
         # Actual keywords assigned to abstract from the references file (uncontrolled references).
-        self.keywords = None
+        self.actual_keywords = self.gen_actual_keywords(keyword_reference)
 
     def gen_abstract(self):
         """ Generates the string representation of the abstract from the XML file.
@@ -31,9 +33,11 @@ class Abstract:
         # Reconstruct original paragraph via tokens, add or remove spaces, and puncuation as required.
         PUNCUATION_POS_TAGS = [",", ".", ":"]
         REPLACEMENTS  = {
-            "-LRB-": "(",
+            "-LRB-": " (",
             "-RRB-": ")"
         }
+        PREV_CHAR_TO_NOT_ADD_SPACES_TO = ["("]
+
         output = ""
         for sentence_tuple_list in sentence_tokens:
             sentence = ""
@@ -50,36 +54,60 @@ class Abstract:
     
                 else:
                     if sentence:
-                        sentence += " " + word
+                        if sentence[-1] in PREV_CHAR_TO_NOT_ADD_SPACES_TO:
+                            sentence += word
+                        else:
+                            sentence += " " + word
                     else:
                         sentence += word
 
             # Some sentences do not actually end on a period. Add if necessary.
             if sentence[-1] != ".":
-                sentence += "."
+                sentence += ". "
+            if sentence[-1] == ".":
+                sentence += " "
 
             output += sentence
 
         # Output both the parsed series of tokens (for debug) and the actual abstract paragraph.
         return (sentence_tokens, output)
 
+    def gen_actual_keywords(self,keyword_reference):
+        """ Helper function, extracts the "true" keywords from the keyword reference JSON file object.
+        """
+        output = []
+        for nested_list in keyword_reference[self.num]:
+            assert(len(nested_list) == 1)
+            output.append(nested_list[0])
+
+        return output
 
 
-JSON_FILE_PATH = r".\data\references.test.uncontr.json"  
-XML_FILE_PATH  = r".\data\test"
 
-abstracts = []
-for  root, dirs, files in os.walk(XML_FILE_PATH):
-    for f in files:
-        fullpath = os.path.join(root, f)
-        if fullpath.endswith(".xml"):
-            abstracts.append(Abstract(fullpath))
+def getAbstracts():
+    """ Loader function, used to get object representations of all of the test data-folder abstracts.
+    """
+    JSON_FILE_PATH = r".\data\references\test.uncontr.json"  
+    XML_FILE_PATH  = r".\data\test"
 
-for a in abstracts:
-    print(a.filepath)
-    print(a.num)
-    print(a.abstract)
+    keyword_reference = json.load(open(JSON_FILE_PATH, "r", encoding="utf-8"))
 
-    x = input()
-    if x:
-        break
+    abstracts = []
+    for  root, dirs, files in os.walk(XML_FILE_PATH):
+        for f in files:
+            fullpath = os.path.join(root, f)
+            if fullpath.endswith(".xml"):
+                abstracts.append(Abstract(fullpath, keyword_reference))
+
+    return abstracts
+
+if __name__ == "__main__":
+    for a in getAbstracts():
+        print(a.filepath)
+        print(a.num)
+        print(a.abstract)
+        print(a.actual_keywords)
+        
+        x = input()
+        if x:
+            break
