e5e607747f11988cf60c8ed9db9d7ef24b9acdc8
Shouyang Zhou <s.zhou1511@gmail.com>
2019-11-24 17:02:46 -0700

    Added basic TextRank Functionality

--- scripts/benchmark.py
+++ scripts/benchmark.py
@@ -1,6 +1,12 @@
 import nltk
-from loadData import getAbstracts
-from rake_nltk import Rake
+import statistics
+
+from loadData    import getAbstracts
+from rake_nltk   import Rake
+from summa       import keywords
+
+
+FOX_SL= ['a', 'about', 'above', 'across', 'after', 'again', 'against', 'all', 'almost', 'alone', 'along', 'already', 'also', 'although', 'always', 'among', 'an', 'and', 'another', 'any', 'anybody', 'anyone', 'anything', 'anywhere', 'are', 'area', 'areas', 'around', 'as', 'ask', 'asked', 'asking', 'asks', 'at', 'away', 'b', 'back', 'backed', 'backing', 'backs', 'be', 'because', 'become', 'becomes', 'became', 'been', 'before', 'began', 'behind', 'being', 'beings', 'best', 'better', 'between', 'big', 'both', 'but', 'by', 'c', 'came', 'can', 'cannot', 'case', 'cases', 'certain', 'certainly', 'clear', 'clearly', 'come', 'could', 'd', 'did', 'differ', 'different', 'differently', 'do', 'does', 'done', 'down', 'downed', 'downing', 'downs', 'during', 'e', 'each', 'early', 'either', 'end', 'ended', 'ending', 'ends', 'enough', 'even', 'evenly', 'ever', 'every', 'everybody', 'everyone', 'everything', 'everywhere', 'f', 'face', 'faces', 'fact', 'facts', 'far', 'felt', 'few', 'find', 'finds', 'first', 'for', 'four', 'from', 'full', 'fully', 'further', 'furthered', 'furthering', 'furthers', 'g', 'gave', 'general', 'generally', 'get', 'gets', 'give', 'given', 'gives', 'go', 'going', 'good', 'goods', 'got', 'great', 'greater', 'greatest', 'group', 'grouped', 'grouping', 'groups', 'h', 'had', 'has', 'have', 'having', 'he', 'her', 'herself', 'here', 'high', 'higher', 'highest', 'him', 'himself', 'his', 'how', 'however', 'i', 'if', 'important', 'in', 'interest', 'interested', 'interesting', 'interests', 'into', 'is', 'it', 'its', 'itself', 'j', 'just', 'k', 'keep', 'keeps', 'kind', 'knew', 'know', 'known', 'knows', 'l', 'large', 'largely', 'last', 'later', 'latest', 'least', 'less', 'let', 'lets', 'like', 'likely', 'long', 'longer', 'longest', 'm', 'made', 'make', 'making', 'man', 'many', 'may', 'me', 'member', 'members', 'men', 'might', 'more', 'most', 'mostly', 'mr', 'mrs', 'much', 'must', 'my', 'myself', 'n', 'necessary', 'need', 'needed', 'needing', 'needs', 'never', 'new', 'newer', 'newest', 'next', 'no', 'non', 'not', 'nobody', 'noone', 'nothing', 'now', 'nowhere', 'number', 'numbers', 'o', 'of', 'off', 'often', 'old', 'older', 'oldest', 'on', 'once', 'one', 'only', 'open', 'opened', 'opening', 'opens', 'or', 'order', 'ordered', 'ordering', 'orders', 'other', 'others', 'our', 'out', 'over', 'p', 'part', 'parted', 'parting', 'parts', 'per', 'perhaps', 'place', 'places', 'point', 'pointed', 'pointing', 'points', 'possible', 'present', 'presented', 'presenting', 'presents', 'problem', 'problems', 'put', 'puts', 'q', 'quite', 'r', 'rather', 'really', 'right', 'room', 'rooms', 's', 'said', 'same', 'saw', 'say', 'says', 'second', 'seconds', 'see', 'sees', 'seem', 'seemed', 'seeming', 'seems', 'several', 'shall', 'she', 'should', 'show', 'showed', 'showing', 'shows', 'side', 'sides', 'since', 'small', 'smaller', 'smallest', 'so', 'some', 'somebody', 'someone', 'something', 'somewhere', 'state', 'states', 'still', 'such', 'sure', 't', 'take', 'taken', 'than', 'that', 'the', 'their', 'them', 'then', 'there', 'therefore', 'these', 'they', 'thing', 'things', 'think', 'thinks', 'this', 'those', 'though', 'thought', 'thoughts', 'three', 'through', 'thus', 'to', 'today', 'together', 'too', 'took', 'toward', 'turn', 'turned', 'turning', 'turns', 'two', 'u', 'under', 'until', 'up', 'upon', 'us', 'use', 'uses', 'used', 'v', 'very', 'w', 'want', 'wanted', 'wanting', 'wants', 'was', 'way', 'ways', 'we', 'well', 'wells', 'went', 'were', 'what', 'when', 'where', 'whether', 'which', 'while', 'who', 'whole', 'whose', 'why', 'will', 'with', 'within', 'without', 'work', 'worked', 'working', 'works', 'would', 'y', 'year', 'years', 'yet', 'you', 'young', 'younger', 'youngest', 'your', 'yours']
 
 def get_recall(estimate, actual):
     estimate = set(estimate)
@@ -29,25 +35,91 @@ def get_F_score(estimate, actual, alpha = 0.5):
     return 1.0 / (alpha / precision + (1 - alpha) / recall)
 
 
-T_DENOM = 3
-fox_stoplist=['a', 'about', 'above', 'across', 'after', 'again', 'against', 'all', 'almost', 'alone', 'along', 'already', 'also', 'although', 'always', 'among', 'an', 'and', 'another', 'any', 'anybody', 'anyone', 'anything', 'anywhere', 'are', 'area', 'areas', 'around', 'as', 'ask', 'asked', 'asking', 'asks', 'at', 'away', 'b', 'back', 'backed', 'backing', 'backs', 'be', 'because', 'become', 'becomes', 'became', 'been', 'before', 'began', 'behind', 'being', 'beings', 'best', 'better', 'between', 'big', 'both', 'but', 'by', 'c', 'came', 'can', 'cannot', 'case', 'cases', 'certain', 'certainly', 'clear', 'clearly', 'come', 'could', 'd', 'did', 'differ', 'different', 'differently', 'do', 'does', 'done', 'down', 'downed', 'downing', 'downs', 'during', 'e', 'each', 'early', 'either', 'end', 'ended', 'ending', 'ends', 'enough', 'even', 'evenly', 'ever', 'every', 'everybody', 'everyone', 'everything', 'everywhere', 'f', 'face', 'faces', 'fact', 'facts', 'far', 'felt', 'few', 'find', 'finds', 'first', 'for', 'four', 'from', 'full', 'fully', 'further', 'furthered', 'furthering', 'furthers', 'g', 'gave', 'general', 'generally', 'get', 'gets', 'give', 'given', 'gives', 'go', 'going', 'good', 'goods', 'got', 'great', 'greater', 'greatest', 'group', 'grouped', 'grouping', 'groups', 'h', 'had', 'has', 'have', 'having', 'he', 'her', 'herself', 'here', 'high', 'higher', 'highest', 'him', 'himself', 'his', 'how', 'however', 'i', 'if', 'important', 'in', 'interest', 'interested', 'interesting', 'interests', 'into', 'is', 'it', 'its', 'itself', 'j', 'just', 'k', 'keep', 'keeps', 'kind', 'knew', 'know', 'known', 'knows', 'l', 'large', 'largely', 'last', 'later', 'latest', 'least', 'less', 'let', 'lets', 'like', 'likely', 'long', 'longer', 'longest', 'm', 'made', 'make', 'making', 'man', 'many', 'may', 'me', 'member', 'members', 'men', 'might', 'more', 'most', 'mostly', 'mr', 'mrs', 'much', 'must', 'my', 'myself', 'n', 'necessary', 'need', 'needed', 'needing', 'needs', 'never', 'new', 'newer', 'newest', 'next', 'no', 'non', 'not', 'nobody', 'noone', 'nothing', 'now', 'nowhere', 'number', 'numbers', 'o', 'of', 'off', 'often', 'old', 'older', 'oldest', 'on', 'once', 'one', 'only', 'open', 'opened', 'opening', 'opens', 'or', 'order', 'ordered', 'ordering', 'orders', 'other', 'others', 'our', 'out', 'over', 'p', 'part', 'parted', 'parting', 'parts', 'per', 'perhaps', 'place', 'places', 'point', 'pointed', 'pointing', 'points', 'possible', 'present', 'presented', 'presenting', 'presents', 'problem', 'problems', 'put', 'puts', 'q', 'quite', 'r', 'rather', 'really', 'right', 'room', 'rooms', 's', 'said', 'same', 'saw', 'say', 'says', 'second', 'seconds', 'see', 'sees', 'seem', 'seemed', 'seeming', 'seems', 'several', 'shall', 'she', 'should', 'show', 'showed', 'showing', 'shows', 'side', 'sides', 'since', 'small', 'smaller', 'smallest', 'so', 'some', 'somebody', 'someone', 'something', 'somewhere', 'state', 'states', 'still', 'such', 'sure', 't', 'take', 'taken', 'than', 'that', 'the', 'their', 'them', 'then', 'there', 'therefore', 'these', 'they', 'thing', 'things', 'think', 'thinks', 'this', 'those', 'though', 'thought', 'thoughts', 'three', 'through', 'thus', 'to', 'today', 'together', 'too', 'took', 'toward', 'turn', 'turned', 'turning', 'turns', 'two', 'u', 'under', 'until', 'up', 'upon', 'us', 'use', 'uses', 'used', 'v', 'very', 'w', 'want', 'wanted', 'wanting', 'wants', 'was', 'way', 'ways', 'we', 'well', 'wells', 'went', 'were', 'what', 'when', 'where', 'whether', 'which', 'while', 'who', 'whole', 'whose', 'why', 'will', 'with', 'within', 'without', 'work', 'worked', 'working', 'works', 'would', 'y', 'year', 'years', 'yet', 'you', 'young', 'younger', 'youngest', 'your', 'yours']
-abstracts = getAbstracts()
+def get_RAKE_keywords(sample, stoplist, T_DENOM = 3):
 
-for sample in abstracts:
-    rake = Rake(stopwords = fox_stoplist)
+    rake = Rake(stopwords = stoplist)
     rake.extract_keywords_from_text(sample.abstract)
 
     t = len(rake.get_word_degrees()) // T_DENOM
 
-    estimate_keywords  = rake.get_ranked_phrases()[:t]
-    actual_keywords    = sample.actual_keywords
+    return rake.get_ranked_phrases()[:t]
 
-    print(sample.abstract)
-    print(estimate_keywords)
-    print(actual_keywords)
-    print(get_recall(estimate_keywords,actual_keywords), get_precision(estimate_keywords, actual_keywords))
-    print("--")
+def get_TextRank_keywords(sample, window = 2, T_DEMON = 3):
+    keywords.WINDOW_SIZE = window
+    return keywords.keywords(sample.abstract, ratio = 1 / T_DEMON, split= True)
 
-    x = input()
-    if x:
-        break
+def tabluate(method_name, estimate_keywords, actual_keywords, output):
+    # Update Assigned Keywords
+    key = (method_name, "Assigned")
+    output[key] = output.get(key, 0) + len(estimate_keywords)
+
+    # Update Total Correct Keywords
+    estimate = set(estimate_keywords)
+    actual   = set(actual_keywords)
+
+    correct_keywords = len(estimate.intersection(actual))
+
+    key = (method_name, "Correct")
+    output[key] = output.get(key, 0) + correct_keywords
+
+    # Update Precision
+    key = (method_name, "Precision")
+
+    temp = output.get(key, [])
+    temp.append(get_precision(estimate_keywords, actual_keywords))
+  
+    output[key] = temp
+
+    # Update Recall
+    key = (method_name, "Recall")
+
+    temp = output.get(key, [])
+    temp.append(get_recall(estimate_keywords, actual_keywords))
+
+    output[key] = temp
+
+    
+abstracts = getAbstracts()
+counts = {}
+
+for sample in abstracts:
+    # Get Actual Keywords
+    actual_keywords           = sample.actual_keywords
+    
+    # Get Estimates
+    rake_estimate_keywords     = get_RAKE_keywords(sample, FOX_SL)
+    textrank2_estimate_keywords = get_TextRank_keywords(sample, window = 2)
+    textrank3_estimate_keywords = get_TextRank_keywords(sample, window = 3)
+    
+    # Tabluate Estimates
+    tabluate("RAKE-FOX", rake_estimate_keywords, actual_keywords, counts)
+    tabluate("TEXT-2", textrank2_estimate_keywords, actual_keywords, counts)
+    tabluate("TEXT-3", textrank3_estimate_keywords, actual_keywords, counts)
+
+    # Generate Output
+    # print("Abstract:", sample.abstract[:75])
+    # print("RAKE:", rake_estimate_keywords)
+    # print("TextRank WINDOW = 2: ", textrank2_estimate_keywords)
+    # print("TextRank WINDOW = 3: ", textrank3_estimate_keywords)
+    # print("Actual Keywords: ", actual_keywords)
+    # print(get_recall(rake_estimate_keywords,actual_keywords), get_precision(rake_estimate_keywords, actual_keywords))
+    # print("--")
+
+    # x = input()
+    # if x:
+        # break
+
+print(counts[("RAKE-FOX", "Assigned")])
+print(counts[("RAKE-FOX", "Correct")])
+print(statistics.mean(counts[("RAKE-FOX","Precision")]))
+print(statistics.mean(counts[("RAKE-FOX","Recall")]))
+print("--")
+print(counts[("TEXT-2", "Assigned")])
+print(counts[("TEXT-2", "Correct")])
+print(statistics.mean(counts[("TEXT-2","Precision")]))
+print(statistics.mean(counts[("TEXT-2","Recall")]))
+print("--")
+print(counts[("TEXT-3", "Assigned")])
+print(counts[("TEXT-3", "Correct")])
+print(statistics.mean(counts[("TEXT-3","Precision")]))
+print(statistics.mean(counts[("TEXT-3","Recall")]))
